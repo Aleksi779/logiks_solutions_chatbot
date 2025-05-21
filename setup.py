609307894c1423f849b2177fd8f863b01cf7adcc
Aleksi779 <aleqsimodebadze777@gmail.com>
2025-05-21 20:54:36 +0400

    Add folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Aleksi Modebadze',
+    author_email='aleqsimodebadze777@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
